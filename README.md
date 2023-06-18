cee075b2d8916f450af0095be2185341d9f90307
Okoli Ekene <khennyphresh@gmail.com>
2023-06-18 02:31:57 +0100

    meta verse

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MagVerse
+First realese
+This is just for learning no big deal about it
---
+++ index.js
@@ -0,0 +1,20 @@
+function pressMenu() {
+    let x = document.getElementById('links');
+
+if (x.style.display === "block") {
+    x.style.display = "none";
+}
+else {
+    x.style.display = "block";
+}   
+}
+
+function pressSearch() {
+    let x = document.getElementById('search');
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block"
+    }
+}
